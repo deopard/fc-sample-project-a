b7d5447d9b0664f3d8d2bd5cb940c7bdca1405e3
Tom Kim <tom.kim@dramancompany.com>
2017-10-07 17:13:11 +0900

    added /health, port listener

--- app.js
+++ app.js
@@ -5,3 +5,10 @@ app.get('/', (req, res) => {
   res.send('A project입니다!');
 });
 
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
+
+app.get('/health', (req, res) => {
+  res.send('Healthy');
+});
